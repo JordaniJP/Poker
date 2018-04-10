5c8fccbcd77276826356eaf95702d91dd0f574f8
Michael <mrada6@gmail.com>
2018-04-10 08:11:24 -0400

    If you try to draw a card from an empty deck.  A new deck is made first

--- Deck.py
+++ Deck.py
@@ -16,8 +16,17 @@ class Deck:
         '''
         This method will return a random card from the deck.
         This card will be removed from the deck and can not 
-        be drawn again.
+        be drawn again.  If the deck is empty it will shuffle
+        up a new deck and draw from that
         '''
+        #check to see if the deck is empty
+        if(len(self.deck)<=0):
+            #if empty create a new deck
+            self.deck =[]
+            for x in range(len(Card.suits)):
+                for y in range(len(Card.ranks)):
+                    temp = Card.Card(y,x)
+                    self.deck.append(temp)
         rand = random.randint(0,len(self.deck)-1)
         # this will get the random card
         card = self.deck[rand]
