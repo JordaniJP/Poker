6744b28bee630f9edb2fad2c86fcba17b33861e8
JordaniJP <35529785+JordaniJP@users.noreply.github.com>
2018-04-14 21:43:10 -0400

    Added new sort method

--- Hand.py
+++ Hand.py
@@ -1,4 +1,5 @@
 import Deck
+from Card import Card
 class Hand:
     def __init__(self, inputDeck):
         self.deck=inputDeck
@@ -9,7 +10,7 @@ class Hand:
         self.hand.append(self.deck.dealCard())
         self.hand.append(self.deck.dealCard())
         self.hand.append(self.deck.dealCard())
-        sorted(hand.hand, key=Card.getRank())
+        sorted(self.hand, key=Card.getRank)
     def setHand(self, cardOne,cardTwo,cardThree,cardFour,cardFive):
         self.hand =[]
         self.append(cardOne)
@@ -17,25 +18,36 @@ class Hand:
         self.append(cardThree)
         self.append(cardFour)
         self.append(cardFive)
-        def getPokerHand():
-            if 14 and 13 and 12 and 11 and 10 in self.hand:
-                return 10
-            elif self.hand[1] == self.hand[0]+1 and self.hand[2] == self.hand[1]+1 and self.hand[3]==self.hand[2]+1 and self.hand[4]==self.hand[3]+1:
+    def getPokerHand(self):
+        rankings = list(map(Card.getRank, self.hand))
+        suits = list(map(Card.getSuit, self.hand))
+        if 14 and 13 and 12 and 11 and 10 in rankings:
+            return 10
+        else:
+            return 1
+        
+        
+        
+        
+        
+        
+        '''
+        elif self.hand[1] == self.hand[0]+1 and self.hand[2] == self.hand[1]+1 and self.hand[3]==self.hand[2]+1 and self.hand[4]==self.hand[3]+1:
                 return 5
-            elif self.hand[2] == self.hand[1]+1 and self.hand[3]==self.hand[2]+1 and self.hand[4]==self.hand[3]+1:
+        elif self.hand[2] == self.hand[1]+1 and self.hand[3]==self.hand[2]+1 and self.hand[4]==self.hand[3]+1:
                 return 5
-            elif self.hand[0] == self.hand[1] and self.hand[1] == self.hand[2] and self.hand[2] == self.hand[3]:
+        elif self.hand[0] == self.hand[1] and self.hand[1] == self.hand[2] and self.hand[2] == self.hand[3]:
                 return 8
-            elif self.hand[1] == self.hand[2] and self.hand[2] == self.hand[3] and self.hand[3] == self.hand[4]:
+        elif self.hand[1] == self.hand[2] and self.hand[2] == self.hand[3] and self.hand[3] == self.hand[4]:
                 return 8   
-            elif self.hand[0] == self.hand[1] and self.hand[1] == self.hand[2]:
+        elif self.hand[0] == self.hand[1] and self.hand[1] == self.hand[2]:
                 return 4
-            elif self.hand[1] == self.hand[2] and self.hand[2] == self.hand[3]:
+        elif self.hand[1] == self.hand[2] and self.hand[2] == self.hand[3]:
                 return 4
-            elif self.hand[2] == self.hand[3] and self.hand[3] == self.hand[4]:  
+        elif self.hand[2] == self.hand[3] and self.hand[3] == self.hand[4]:  
                 return 4
-            elif self.hand[0] == self.hand[1] or self.hand[1] == self.hand[2] or self.hand[2] == self.hand[3] or self.hand[3] == self.hand[4]:
-                return 2   
+        elif self.hand[0] == self.hand[1] or self.hand[1] == self.hand[2] or self.hand[2] == self.hand[3] or self.hand[3] == self.hand[4]:
+                return 2   '''
             
         
         
